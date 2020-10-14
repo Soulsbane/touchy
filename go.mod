010406fdcf57ad7919ded4d154722eeaa0d90202
Paul Crane <paul@acheronsoft.com>
2020-10-14 02:18:56 -0500

    Initial command line argument handling.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Soulsbane/touchy
 
 go 1.15
+
+require (
+	github.com/alexflint/go-arg v1.3.0
+	github.com/emirpasic/gods v1.12.0 // indirect
+)
--- touchy.go
+++ touchy.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/alexflint/go-arg"
+)
 
+func loadTemplate(templateName string) {
+}
+
+func main() {
+	var args struct {
+		TemplateName string `arg:"positional, required"`
+	}
+
+	arg.MustParse(&args)
+	loadTemplate(args.TemplateName)
 }
