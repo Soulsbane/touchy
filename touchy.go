fdca8c7abf2ad802e0eb0d8eeb02ccc9b536cf01
Paul Crane <paul@acheronsoft.com>
2020-10-15 03:41:18 -0500

    Initial template loading functions.

--- touchy.go
+++ touchy.go
@@ -1,10 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
 	"github.com/alexflint/go-arg"
+	"github.com/gobuffalo/packr"
 )
 
-func loadTemplate(templateName string) {
+func loadTemplate(templateName string) string {
+	language := templateName
+	template := "default"
+
+	if strings.Contains(templateName, ".") {
+		var parts = strings.Split(templateName, ".")
+
+		language = parts[0]
+		template = parts[1]
+
+		if template == "" {
+			template = "default"
+		}
+	}
+
+	box := packr.NewBox("./templates")
+	data, err := box.FindString(language + "/" + template + "." + language)
+
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	return data
+}
+
+func buildTemplateList() {
+	files, err := ioutil.ReadDir("templates")
+
+	if err != nil {
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
 }
 
 func main() {
@@ -13,5 +52,5 @@ func main() {
 	}
 
 	arg.MustParse(&args)
-	loadTemplate(args.TemplateName)
+	fmt.Println(loadTemplate(args.TemplateName))
 }
