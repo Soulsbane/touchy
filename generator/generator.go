334452a9c204592443835b57d7a9cb21f7867593
Paul Crane <paul@acheronsoft.com>
2020-11-07 22:58:03 -0600

    Move template generation code to its own package.

---
+++ generator/generator.go
@@ -0,0 +1,95 @@
+package generator
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/gobuffalo/packr"
+)
+
+type languageConfig struct {
+	Name        string
+	Description string
+	Extension   string
+}
+
+func loadLanguageConfig(languageName string) languageConfig {
+	exePath, _ := os.Executable()
+	configFileName := filepath.Join(filepath.Dir(exePath), "/templates/", languageName, "/config.toml")
+
+	data, err := ioutil.ReadFile(configFileName)
+	config := languageConfig{}
+
+	if err != nil {
+		log.Fatal(errors.New("Failed to load config file: " + configFileName))
+	}
+
+	err = toml.Unmarshal(data, &config)
+
+	if err != nil {
+		log.Fatal(errors.New("Failed to read config file: " + configFileName))
+	}
+
+	return config
+}
+
+func loadTemplate(name string) (string, languageConfig) {
+	language := name
+	template := "default"
+	box := packr.NewBox("./templates")
+
+	if strings.Contains(name, ".") {
+		var parts = strings.Split(name, ".")
+
+		language = parts[0]
+		template = parts[1]
+
+		if template == "" {
+			template = "default"
+		}
+	}
+
+	config := loadLanguageConfig(language)
+	templateName := language + "/" + template + "." + config.Extension
+	data, err := box.FindString(templateName)
+
+	if err != nil {
+		//log.Fatal(errors.New("That template does not exist: " + config.Name + " => " + template))
+		log.Fatal(errors.New("That template does not exist: " + name))
+	}
+
+	return data, config
+}
+
+func buildTemplateList() {
+	files, err := ioutil.ReadDir("templates")
+
+	if err != nil {
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+}
+
+// CreateTemplate Creates a template
+func CreateTemplate(fileName string, templateName string) {
+	template, config := loadTemplate(templateName)
+	currentDir, _ := os.Getwd()
+	file, err := os.Create(filepath.Join(currentDir, "/"+fileName+"."+config.Extension))
+
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file.WriteString(template)
+	fmt.Println(template)
+}
--- go.mod
+++ go.mod
@@ -3,20 +3,11 @@ module github.com/Soulsbane/touchy
 go 1.15
 
 require (
+	github.com/BurntSushi/toml v0.3.1
 	github.com/alexflint/go-arg v1.3.0
-	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/gobuffalo/envy v1.9.0 // indirect
+	github.com/gobuffalo/packd v1.0.0 // indirect
 	github.com/gobuffalo/packr v1.30.1
-	github.com/gobuffalo/packr/v2 v2.8.0 // indirect
-	github.com/huandu/xstrings v1.3.2 // indirect
-	github.com/karrick/godirwalk v1.16.1 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
-	github.com/pelletier/go-toml v1.8.1
 	github.com/rogpeppe/go-internal v1.6.2 // indirect
-	github.com/sirupsen/logrus v1.7.0 // indirect
-	github.com/spf13/cobra v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee // indirect
-	golang.org/x/sync v0.0.0-20201008141435-b3e1573b7520 // indirect
-	golang.org/x/sys v0.0.0-20201014080544-cc95f250f6bc // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
 )
