db70ab84db6b3ee1f71b2d2b1e7dff70884b302c
Paul Crane <paul@acheronsoft.com>
2020-11-11 04:17:09 -0600

    Create a description of application when -h option is passed.

--- touchy.go
+++ touchy.go
@@ -3,14 +3,20 @@ package main
 import (
 	"github.com/Soulsbane/touchy/generator"
 	"github.com/alexflint/go-arg"
+	"github.com/fatih/color"
 )
 
+type args struct {
+	TemplateName string `arg:"positional, required"`
+	FileName     string `arg:"-n, --name" default:"template" help:"Name of the generated file."`
+}
+
+func (args) Description() string {
+	return color.GreenString("Create a file based upon a template.")
+}
+
 func main() {
-	var args struct {
-		//FileName     string `arg:"positional, required"`
-		TemplateName string `arg:"positional, required"`
-		FileName     string `arg:"-n, --name" default:"template" help:"Name of the generated file."`
-	}
+	var args args
 
 	arg.MustParse(&args)
 	generator.CreateFileFromTemplate(args.FileName, args.TemplateName)
