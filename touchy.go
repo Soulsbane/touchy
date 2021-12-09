6046c0848a4bea8ff105fbf1f568a6a205561c4d
Paul Crane <paul@acheronsoft.com>
2021-12-08 21:36:21 -0600

    Add Show and List subcommands to args

--- touchy.go
+++ touchy.go
@@ -9,6 +9,8 @@ import (
 type args struct {
 	TemplateName string `arg:"positional, required"`
 	FileName     string `arg:"-n, --name" help:"Name of the generated file. If not specified, the filename will be the config's DefaultFileName value."`
+	Show         string `arg:"subcommand:show" help:"Show the contents of the template file"`
+	List         string `arg:"subcommand:list" help:"Show a list of all installed templates"`
 }
 
 func (args) Description() string {
