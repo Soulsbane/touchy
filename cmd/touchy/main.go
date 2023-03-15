b8c0de8b9eef4be08051a1ee85aa8530fac8c6a4
Paul Crane <paul@acheronsoft.com>
2023-03-14 23:11:30 -0500

    Add function isReservedCommand

--- cmd/touchy/main.go
+++ cmd/touchy/main.go
@@ -3,10 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"reflect"
+	"strings"
 
 	"github.com/Soulsbane/touchy/internal/templates"
 	"github.com/alexflint/go-arg"
 )
+
+func isReservedCommand(cmds commands, command string) bool {
+	// This checks if a command is reserved based on the command existing as a member of the commands struct.
+	dummyVal := reflect.ValueOf(cmds)
+	numFields := dummyVal.NumField()
+
+	for i := 0; i < numFields; i++ {
+		field := dummyVal.Type().Field(i).Name
+
+		if strings.ToLower(field) == command {
+			return true
+		}
+	}
+
+	return false
+}
 
 func main() {
 	var cmds commands
@@ -14,18 +32,17 @@ func main() {
 
 	if len(cmdLineArgs) == 0 {
 		fmt.Println("No arguments provided. Use -h or --help for more information.")
+	} else {
+		arg.MustParse(&cmds)
+		languages := templates.New()
+
+		switch {
+		case cmds.Create != nil:
+			languages.CreateFileFromTemplate(cmds.Create.Language, cmds.Create.TemplateName, cmds.Create.FileName)
+		case cmds.List != nil:
+			languages.List(cmds.List.Language)
+		case cmds.Show != nil:
+			languages.ShowTemplate(cmds.Show.Language, cmds.Show.TemplateName)
+		}
 	}
-
-	arg.MustParse(&cmds)
-	languages := templates.New()
-
-	switch {
-	case cmds.Create != nil:
-		languages.CreateFileFromTemplate(cmds.Create.Language, cmds.Create.TemplateName, cmds.Create.FileName)
-	case cmds.List != nil:
-		languages.List(cmds.List.Language)
-	case cmds.Show != nil:
-		languages.ShowTemplate(cmds.Show.Language, cmds.Show.TemplateName)
-	}
-
 }
