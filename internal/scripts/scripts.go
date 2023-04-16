15dc49102ecff3ceb33b950cb0aab186ca2e64d7
Paul Crane <paul@acheronsoft.com>
2023-04-15 22:51:46 -0500

    Add initial TouchyScripts support type and initialization

--- internal/scripts/scripts.go
+++ internal/scripts/scripts.go
@@ -1,1 +1,14 @@
 package scripts
+
+import "github.com/Soulsbane/goscriptsystem/goscriptsystem"
+
+type TouchyScripts struct {
+	scriptSystem *goscriptsystem.ScriptSystem
+}
+
+func New() *TouchyScripts {
+	var touchyScripts TouchyScripts
+
+	touchyScripts.scriptSystem = goscriptsystem.New(goscriptsystem.NewScriptErrors())
+	return &touchyScripts
+}
