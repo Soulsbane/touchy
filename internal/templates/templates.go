f7f6e44572ea7514c056a0b45daedd523b190347
Paul Crane <paul@acheronsoft.com>
2023-05-19 03:47:00 -0500

    Fix typo
    Took 7 minutes

--- internal/templates/templates.go
+++ internal/templates/templates.go
@@ -18,7 +18,7 @@ import (
 var embedsDir embed.FS
 
 type Language struct {
-	// dirName         string                  // Name of the directory under the templates directory.
+	// dirName         string                  // Name of the directory under the template's directory.
 	infoConfig      infofile.InfoFile            // Each language has a config file in its root directory call config.toml
 	templateConfigs map[string]infofile.InfoFile // A list of all the templates in the language directory. The key is the template dir name.
 }
