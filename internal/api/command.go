52c43c50c87a3bb545e4ab7247868ec8ae91e91b
Paul Crane <paul@acheronsoft.com>
2025-06-21 20:44:30 -0500

    Add API function Command.Run

--- internal/api/command.go
+++ internal/api/command.go
@@ -1,8 +1,21 @@
 package api
 
+import "os/exec"
+
 type Command struct {
 }
 
 func NewCommand() *Command {
 	return &Command{}
+}
+
+func (c *Command) Run(command string, args ...string) error {
+	cmd := exec.Command(command, args...)
+	err := cmd.Run()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
